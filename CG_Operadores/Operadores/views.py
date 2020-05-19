25150c1d6005405049671f6ade7e6cfac5e5537d
Jaime Torres <jaimetr97@gmail.com>
2020-05-19 10:34:06 -0500

    subir_imagen_incluida

--- CG_Operadores/Operadores/views.py
+++ CG_Operadores/Operadores/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 from pprint import pprint
-
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
-def index(request):
 
-    # pprint(dir(request))
+def index(request):
+    if request.method == 'POST':
+        myfile = request.FILES['Original']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return redirect(reverse('Ops:operators', kwargs={'name': filename}))
     return render(request, 'Pages/index.html')
+
+
+def operators(request, name):
+    print(name)
+    return render(request, 'Pages/Image_Visualization.html')
