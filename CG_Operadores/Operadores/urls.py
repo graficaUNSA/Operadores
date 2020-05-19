5deffa2868c1558a8e00730c6d0dee2caebc937f
Jaime Torres <jaimetr97@gmail.com>
2020-05-19 09:43:44 -0500

    page_incluida

--- CG_Operadores/Operadores/urls.py
+++ CG_Operadores/Operadores/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'Operations'
 
 urlpatterns = [
     path('', views.index, name='index'),
--- CG_Operadores/Operadores/views.py
+++ CG_Operadores/Operadores/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pprint import pprint
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    # pprint(dir(request))
+    return render(request, 'Pages/index.html')
